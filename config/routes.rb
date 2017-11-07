e91d0817d3e18f67691814045aa7f11a2893a101
Sophie Dermaux <sophiebcdermaux@gmail.com>
2017-11-07 17:23:54 +0000

    first push of Q1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# get all tasks
+  get "tasks", to: "tasks#index"
+#add new tasks
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+#display one task
+  get "tasks/:id", to: "tasks#show", as: :task
+#update a task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+#delete a task
+  delete "tasks/:id", to: "tasks#destroy"
+# resources :tasks
 end
